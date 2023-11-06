de2ba7b9bf4301814e1c5f5a2b9699a10445fb01
Govind <govind151201@gmail.com>
2023-11-06 18:33:12 +0530

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <footer>
+      <div>
+        <h1>Panica</h1>
+        <p>@all right reserved</p>
+      </div>
+
+      <div>
+        <h5>Follow Us</h5>
+        <div>
+          <a href="https://www.youtube.com/channel/UCM9J06DV8q73PIs4IyiJmKA" target={"blank"}>
+            Youtube
+          </a>
+          <a href="https://www.instagram.com/govind_rks" target={"blank"}>
+            Instagram
+          </a>
+          <a href="https://github.com/govindrks" target={"blank"}>
+            Github
+          </a>
+        </div>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
